c473577cadee020f94b43abbdbf07e1149c0b7f8
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2022-06-02 10:45:13 +0200

    removed error in reporter and exporter due to naming

--- pyroll/sims_power_and_labour/reporter_hookimpls.py
+++ pyroll/sims_power_and_labour/reporter_hookimpls.py
@@ -7,6 +7,6 @@ from pyroll.utils import for_units
 @for_units(RollPass)
 def unit_properties(unit: RollPass):
     return dict(
-        lever_arm_coefficient_sims=f"{unit.lever_arm_weber:.2f}",
-        roll_torque_general=f"{unit.roll.roll_torque_weber:.2f}"
+        lever_arm_coefficient_sims=f"{unit.lever_arm_sims:.2f}",
+        roll_torque_general=f"{unit.roll.roll_torque_general:.2f}"
     )
