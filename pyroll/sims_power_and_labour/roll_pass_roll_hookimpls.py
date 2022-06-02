dd92ae71a0a17cf162d61f465f73d6d92ddcc774
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2022-06-02 10:47:17 +0200

    resolved naming error

--- pyroll/sims_power_and_labour/roll_pass_roll_hookimpls.py
+++ pyroll/sims_power_and_labour/roll_pass_roll_hookimpls.py
@@ -3,4 +3,4 @@ from pyroll.core import RollPass
 
 @RollPass.Roll.hookimpl
 def roll_torque_general(roll_pass: RollPass, roll: RollPass.Roll):
-    return roll_pass.roll_force * roll.contact_length * roll_pass.lever_arm_weber
+    return roll_pass.roll_force * roll.contact_length * roll_pass.lever_arm_sims
